9aad80abb2509bc7252104d02b69618154cf0bff
pspalmirasaraaf <pspalmirasaraaf@gmail.com>
2024-08-14 21:37:10 -0300

    add ecr, remove unused files

---
+++ rds/ecr.tf
@@ -0,0 +1,29 @@
+resource "aws_ecr_repository" "ecr_payment_service" {
+  name = var.payment_service_name
+
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "ecr_customer_service" {
+  name = var.customer_service_name
+
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "ecr_order_service" {
+  name = var.order_service_name
+
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
